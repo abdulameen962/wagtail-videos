a798aefab28476f962a1ee2f4120456568d9440e
abdulameen962 <77135492+abdulameen962@users.noreply.github.com>
2024-03-19 14:51:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     url="https://github.com/neon-jungle/wagtailvideos",
     install_requires=[
         "wagtail>=5.2",
+        "wagtail-modeladmin>=2.0.0"
         "Django>=3.2",
         "django-enumchoicefield>=1.1.0",
         "bcp47==0.0.4",
